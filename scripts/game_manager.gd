9d2eddbf500cca4162f744a4a2fc4ec73ced0e96
Niamh <niamh.linguist@gmail.com>
2024-11-18 02:34:31 +0200

    time bonus applied to final score

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,9 +3,22 @@ extends Node
 class_name GameManager
 
 @onready var score_label: Label = $ScoreLabel
+@onready var congrats_label: Label = $CongratsLabel
 
 var score: int = 0
+var start_time: float = 0.0
 
-func add_point():
-	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+func _ready() -> void:
+	start_time = Time.get_unix_time_from_system()
+
+func add_point() -> void:
+	score += 2
+
+func calculate_final_score() -> void:
+	var end_time: float = Time.get_unix_time_from_system()
+	var elapsed_time: int = roundf(end_time - start_time)
+	var bonus: int = 20 - elapsed_time
+	var final_score = bonus + score if(bonus > 0) else score
+	congrats_label.text = "Hooray! You made it in " + str(elapsed_time) + " seconds!"
+	score_label.text = "Your score: " + str(final_score)
+	

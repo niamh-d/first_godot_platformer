460655b2d549f94a21fec89edfda4a1b202d57c6
Niamh <niamh.linguist@gmail.com>
2024-11-18 11:15:07 +0200

    added big bonus mystery block

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,11 +2,14 @@ extends Node
 
 class_name GameManager
 
+const TIME_BONUS: int = 20
+
 @onready var score_label: Label = $ScoreLabel
 @onready var congrats_label: Label = $CongratsLabel
 
 var score: int = 0
 var start_time: float = 0.0
+var bonus: int = 0
 
 func _ready() -> void:
 	start_time = Time.get_unix_time_from_system()
@@ -17,8 +20,10 @@ func add_point() -> void:
 func calculate_final_score() -> void:
 	var end_time: float = Time.get_unix_time_from_system()
 	var elapsed_time: int = roundf(end_time - start_time)
-	var bonus: int = 20 - elapsed_time
+	bonus += TIME_BONUS - elapsed_time
 	var final_score = bonus + score if(bonus > 0) else score
 	congrats_label.text = "Hooray! You made it in " + str(elapsed_time) + " seconds!"
 	score_label.text = "Your score: " + str(final_score)
 	
+func _on_special_bonus_body_entered(body: Node2D) -> void:
+	bonus += 20
